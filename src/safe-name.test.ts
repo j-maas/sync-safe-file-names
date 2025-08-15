c0d5ac0043c88a41371fbb8480bee66f63417e07
Johannes Maas <github@j-maas.de>
2025-08-15 22:59:54 +0200

    Fix build by removing unused fast-check dependency

--- src/safe-name.test.ts
+++ src/safe-name.test.ts
@@ -1,29 +1,28 @@
-import { describe, expect } from "@jest/globals"
-import { test } from "@fast-check/jest"
+import { describe, expect, test } from "@jest/globals";
 import { getSafeName } from "./safe-name";
 
 describe("getSafeName", () => {
-    test("returns safe name unchanged", () => {
-        const input = "This is a valid filename.md";
-        const result = getSafeName(input);
-        expect(result).toBe(input)
-    });
+	test("returns safe name unchanged", () => {
+		const input = "This is a valid filename.md";
+		const result = getSafeName(input);
+		expect(result).toBe(input);
+	});
 
-    test("replaces unsafe characters", () => {
-        const input = "This is not valid?.md"
-        const result = getSafeName(input);
-        expect(result).toBe("This is not valid-.md")
-    });
+	test("replaces unsafe characters", () => {
+		const input = "This is not valid?.md";
+		const result = getSafeName(input);
+		expect(result).toBe("This is not valid-.md");
+	});
 
-    test("trims outer whitespace", () => {
-        const input = " There is whitespace.md"
-        const result = getSafeName(input)
-        expect(result).toBe("There is whitespace.md")
-    })
+	test("trims outer whitespace", () => {
+		const input = " There is whitespace.md";
+		const result = getSafeName(input);
+		expect(result).toBe("There is whitespace.md");
+	});
 
-    test("allows additional characters", () => {
-        const input = "Fancy (exotic) Ž?.md"
-        const result = getSafeName(input, "(Ž")
-        expect(result).toBe("Fancy (exotic- Ž-.md")
-    })
+	test("allows additional characters", () => {
+		const input = "Fancy (exotic) Ž?.md";
+		const result = getSafeName(input, "(Ž");
+		expect(result).toBe("Fancy (exotic- Ž-.md");
+	});
 });
